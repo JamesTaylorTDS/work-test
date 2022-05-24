5b6930b5b6e659b2ee54a73282c0b52086f67947
JamesTaylorTDS <James.taylor@tdsultra.com>
2022-05-24 10:30:18 +0100

    Common

---
+++ PHP/formatter.php
@@ -0,0 +1,103 @@
+<?php
+$users = [
+    ['firstname'=>'David','lastname'=>'Grantham','email'=>'david.grantham@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Felix','lastname'=>'Beltran','email'=>'Felix.Beltran@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Gareth','lastname'=>'Hillier','email'=>'Gareth.Hillier@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Jay','lastname'=>'Taylor','email'=>'Jay.Taylor@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Louise','lastname'=>'Carolan','email'=>'Louise.Carolan@northgateplc.com'],
+    ['firstname'=>'Michael','lastname'=>'Bedford','email'=>'Michael.Bedford@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Shona','lastname'=>'Sheriton','email'=>'Shona.Sheriton@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Stephanie','lastname'=>'Carr','email'=>'Stephanie.Carr@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Andrea','lastname'=>'Cowburn','email'=>'Andrea.Cowburn@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Daniel','lastname'=>'Humphreys','email'=>'Daniel.Humphreys@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Darren','lastname'=>'Jenkinson','email'=>'Darren.Jenkinson@northgateplc.com'],
+    ['firstname'=>'Dominic','lastname'=>'Reid','email'=>'Dominic.Reid@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Geoff','lastname'=>'Crook','email'=>'Geoff.Crook@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Hayley','lastname'=>'Butcher','email'=>'Hayley.Butcher@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Jason','lastname'=>'Russell','email'=>'Jason.Russell@northgateplc.com'],
+    ['firstname'=>'Julie','lastname'=>'Hargreaves','email'=>'Julie.Hargreaves@northgateplc.com'],
+    ['firstname'=>'Kelly','lastname'=>'Mundy','email'=>'Kelly.Mundy@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Leanne','lastname'=>'Porteous','email'=>'Leanne.Porteous@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Lianne','lastname'=>'Farrell','email'=>'lianne.farrell@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Matthew','lastname'=>'Davy','email'=>'Matthew.Davy@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Melissa','lastname'=>'Armitage','email'=>'Melissa.Armitage@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Michelle','lastname'=>'Jagger','email'=>'Michelle.Jagger@Northgatevehiclehire.co.uk'],
+    ['firstname'=>'Nicola','lastname'=>'Plumbly','email'=>'Nicola.Plumbly@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Ryan','lastname'=>'Mathers','email'=>'Ryan.Mathers@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Stacey','lastname'=>'White','email'=>'Stacey.White@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Steve','lastname'=>'Edwards','email'=>'Steve.Edwards@northgatevehiclehire.co.uk;'],
+    ['firstname'=>'Sue','lastname'=>'Holmes','email'=>'Sue.Holmes@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Tracey','lastname'=>'Morris','email'=>'Tracey.Morris@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Vicki','lastname'=>'Page','email'=>'Vicki.Page@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Chloe','lastname'=>'Beal','email'=>'Chloe.Beal@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Chris','lastname'=>'Jackson','email'=>'Chris.Jackson@northgate.co.uk'],
+    ['firstname'=>'Christopher','lastname'=>'Swindells','email'=>'Christopher.Swindells@northgate.co.uk'],
+    ['firstname'=>'Dan','lastname'=>'Puggmurr','email'=>'Dan.Puggmurr@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Danny','lastname'=>'Robertshaw','email'=>'Danny.Robertshaw@northgateplc.com'],
+    ['firstname'=>'David','lastname'=>'McMullen','email'=>'David.McMullen@northgateplc.com'],
+    ['firstname'=>'Denise','lastname'=>'McElvenny','email'=>'Denise.McElvenny@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Hayley','lastname'=>'Woodward','email'=>'Hayley.Woodward@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Iklil','lastname'=>'Saddieq','email'=>'Iklil.Saddieq@northgate.co.uk'],
+    ['firstname'=>'Jack','lastname'=>'Blackett','email'=>'Jack.Blackett@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Jack','lastname'=>'Johnston','email'=>'Jack.Johnston@northgate.co.uk'],
+    ['firstname'=>'Jasmine','lastname'=>'Davies','email'=>'Jasmine.Davies@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Jolene','lastname'=>'Hunt','email'=>'Jolene.Hunt@northgate.co.uk'],
+    ['firstname'=>'Karl','lastname'=>'Lambton','email'=>'karl.lambton@northgateplc.com'],
+    ['firstname'=>'Laura','lastname'=>'Ferguson','email'=>'Laura.Ferguson@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Lewis','lastname'=>'Roberts','email'=>'Lewis.Roberts@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Matthew','lastname'=>'Bell','email'=>'Matt.Bell@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Paul','lastname'=>'Phillips','email'=>'Paul.Phillips@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Rory','lastname'=>'Hunter','email'=>'Rory.Hunter@northgateplc.com'],
+    ['firstname'=>'Sophie','lastname'=>'Craggs','email'=>'Sophie.Craggs@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Stephen','lastname'=>'Carter','email'=>'Stephen.Carter@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Stewart','lastname'=>'Burrows','email'=>'Stewart.Burrows@northgateplc.com'],
+    ['firstname'=>'Suzy','lastname'=>'Rand','email'=>'Suzy.Rand@northgate.co.uk'],
+    ['firstname'=>'Tim','lastname'=>'Walker','email'=>'Tim.Walker@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Alan','lastname'=>'Haycock','email'=>'Alan.Haycock@northgate.co.uk'],
+    ['firstname'=>'Kelly','lastname'=>'Pickett','email'=>'Kelly.Pickett@northgatevsl.com'],
+    ['firstname'=>'Melanie','lastname'=>'Creedy','email'=>'Melanie.Creedy@reddenorthgate.com'],
+    ['firstname'=>'Michelle','lastname'=>'Lowe','email'=>'michelle.lowe@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Nathan','lastname'=>'Hudson','email'=>'Nathan.Hudson@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Carl','lastname'=>'Clough','email'=>'Carl.Clough@northgatevehiclehire.co.uk'],
+    ['firstname'=>'David','lastname'=>'Buckles','email'=>'David.Buckles@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Gavin','lastname'=>'Easby','email'=>'Gavin.Easby@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Graham','lastname'=>'Harrop','email'=>'Graham.Harrop@northgatevehiclehire.co.uk'],
+    ['firstname'=>'James','lastname'=>'Tottey','email'=>'James.Tottey@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Jamie','lastname'=>'Gallagher','email'=>'Jamie.Gallagher@northgateplc.com'],
+    ['firstname'=>'Joe','lastname'=>'Sidney','email'=>'Joe.Sidney@northgatevehiclehire.co.uk'],
+    ['firstname'=>'John','lastname'=>'Murray','email'=>'John.Murray@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Joseph','lastname'=>'Khoury','email'=>'Joseph.Khoury@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Julian','lastname'=>'Kumah','email'=>'Julian.Kumah@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Kate','lastname'=>'Rawlinson','email'=>'Kate.Rawlinson@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Keith','lastname'=>'Murnaghan','email'=>'Keith.Murnaghan@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Maria','lastname'=>'Vizard','email'=>'Maria.Vizard@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Mark','lastname'=>'Beard','email'=>'Mark.Beard@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Matthew','lastname'=>'Grima','email'=>'Matthew.Grima@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Melanie','lastname'=>'Eaton','email'=>'Melanie.Eaton@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Michael','lastname'=>'Shaw','email'=>'Michael.Shaw@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Nathan','lastname'=>'Hosgood','email'=>'Nathan.Hosgood@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Oliver','lastname'=>'Weir','email'=>'Oliver.Weir@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Paul','lastname'=>'Cox','email'=>'Paul.Cox@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Rebecca','lastname'=>'Mort','email'=>'Rebecca.Mort@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Saqib','lastname'=>'Younis','email'=>'Saqib.Younis@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Scott','lastname'=>'Steadman','email'=>'Scott.Steadman@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Tim','lastname'=>'Emptage','email'=>'Tim.Emptage@northgatevehiclehire.co.uk'],
+    ['firstname'=>'Whitney','lastname'=>'Stewart','email'=>'Whitney.Stewart@northgatevehiclehire.co.uk']
+];
+$company_name = 'Trueman Ltd Demo (Control Plus)';
+$users_not_matching = [];
+foreach($users as $user) {
+    if(array_key_exists('firstname', $user) && array_key_exists('lastname', $user)
+    && array_key_exists('email', $user)) {
+        $firstname = trim($user['firstname']);
+        $lastname = trim($user['lastname']);
+        $email = trim($user['email']);
+        print "php bin/console app:user:upsert -c '".$company_name."' --email '".$email."' -u '".$email."' --firstName='".$firstname."' --lastName='".$lastname."' --enabled=1 \nphp bin/console app:config:user:set '".$email."' 'email.monthly-report' 1 \n";
+    } else {
+        $users_not_matching[] = $user;
+    } 
+}
+if(is_array($users_not_matching) && count($users_not_matching) > 0 ) {
+    echo 'Users that didn\'t match:';
+    print $users_not_matching;
+}
